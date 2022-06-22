3698e604c1fce57bbf9f1c8a142f3f272b271288
DerLed <48630545+DerLed@users.noreply.github.com>
2022-06-22 23:16:08 +0300

    add AccountService

--- src/main/java/ru/lebedev/bank/repository/AccountRepository.java
+++ src/main/java/ru/lebedev/bank/repository/AccountRepository.java
@@ -3,6 +3,9 @@ package ru.lebedev.bank.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.lebedev.bank.domain.entity.Account;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
+import java.util.List;
 
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByClientId(Long ClientId);
+    List<Account> findByClientPhone(String phoneNumber);
 }
---
+++ src/main/java/ru/lebedev/bank/service/AccountService.java
@@ -0,0 +1,22 @@
+package ru.lebedev.bank.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.lebedev.bank.domain.entity.Account;
+import ru.lebedev.bank.repository.AccountRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    public List<Account> findByClientId(Long clientId) {
+        return accountRepository.findByClientId(clientId);
+    }
+
+    public List<Account> findByPhoneNumber(String phoneNumber) {
+        return accountRepository.findByClientPhone(phoneNumber);
+    }
+}
