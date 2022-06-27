b4a206ea14b2d6157e4b5dd8f62712f62c91289d
DerLed <48630545+DerLed@users.noreply.github.com>
2022-06-27 23:57:18 +0300

    valid

--- src/main/java/ru/lebedev/bank/domain/user/User.java
+++ src/main/java/ru/lebedev/bank/domain/user/User.java
@@ -5,6 +5,8 @@ import ru.lebedev.bank.domain.Role;
 import ru.lebedev.bank.domain.Status;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "usr")
@@ -20,22 +22,31 @@ public class User {
     @Column(name = "id")
     private Long id;
 
+    @NotNull(message = "Name incorrect")
+    @NotBlank
     @Column(name = "name")
     private String name;
 
+    @NotNull
+    @NotBlank
     @Column(name = "surname")
     private String surname;
 
+    @NotNull
+    @NotBlank
     @Column(name = "login", nullable = false, unique = true)
     private String login;
 
+    @NotNull
     @Column(name = "password", nullable = false)
     private String password;
 
+    @NotNull
     @Enumerated(value = EnumType.STRING)
     @Column(name = "status")
     private Status status;
 
+    @NotNull
     @Enumerated(value = EnumType.STRING)
     @Column(name = "role")
     private Role role;
