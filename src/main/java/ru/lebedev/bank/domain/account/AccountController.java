08878c34be63cad8cab1887ecf4bf5852934f9a4
DerLed <48630545+DerLed@users.noreply.github.com>
2022-06-30 20:13:05 +0300

    commit

--- src/main/java/ru/lebedev/bank/domain/account/AccountController.java
+++ src/main/java/ru/lebedev/bank/domain/account/AccountController.java
@@ -1,12 +1,76 @@
 package ru.lebedev.bank.domain.account;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<AccountDTO>> findByUserId (@PathVariable Long clientId) {
+        List<AccountDTO> accounts = accountService.findByClientId(clientId);
+        return new ResponseEntity<>(accounts, HttpStatus.OK);
+    }
+
+    @GetMapping("/phoneNumber/{phoneNumber}")
+    public ResponseEntity<List<AccountDTO>> findByPhoneNumber (@PathVariable String phoneNumber) {
+        List<AccountDTO> accounts = accountService.findByPhoneNumber(phoneNumber);
+        return new ResponseEntity<>(accounts, HttpStatus.OK);
+    }
+
+//    @GetMapping("/cardNumber/{cardNumber}")
+//    public ResponseEntity<Optional<AccountDTO>> findByCardNumber (@PathVariable String cardNumber) {
+//        Optional<AccountDTO> account = accountService.findByCardNumber(cardNumber);
+//        return new ResponseEntity<>(account, HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/history/{accountId}")
+//    public ResponseEntity<List<TransactionDTO>> getHistory (@PathVariable Long accountId) {
+//        List<TransactionDTO> transactions = accountService.getHistory(accountId);
+//        return new ResponseEntity<>(transactions, HttpStatus.OK);
+//    }
+
+    @GetMapping
+    public ResponseEntity<List<AccountDTO>> findAll () {
+        List<AccountDTO> accounts = accountService.findAll();
+        return new ResponseEntity<>(accounts, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountDTO> save (@RequestBody @Valid AccountDTO account) {
+        AccountDTO savedAccount = accountService.save(account);
+        return new ResponseEntity<>(savedAccount, HttpStatus.CREATED);
+    }
+
+//    @DeleteMapping("/close/{accountId}")
+//    public ResponseEntity<Void> close (@PathVariable Long accountId) {
+//        accountService.close(accountId);
+//        return ResponseEntity.status(HttpStatus.OK).build();
+//    }
+//
+//    @PutMapping("/cardNumber/{accountId}")
+//    public ResponseEntity<Void> transferMoneyByCardNumber (@PathVariable Long accountId,
+//                                                           @RequestBody String cardNumber,
+//                                                           @RequestParam BigDecimal amount) {
+//        accountService.transferMoneyByCardNumber(accountId, cardNumber, amount);
+//        return ResponseEntity.status(HttpStatus.OK).build();
+//    }
+//
+//    @PutMapping("/phoneNumber/{accountId}")
+//    public ResponseEntity<Void> transferMoneyByUserPhoneNumber (@PathVariable Long accountId,
+//                                                                @RequestBody String phoneNumber,
+//                                                                @RequestParam BigDecimal amount) {
+//        accountService.transferMoneyByUserPhoneNumber(accountId, phoneNumber, amount);
+//        return ResponseEntity.status(HttpStatus.OK).build();
+//    }
 }
