faa9f6492fd2f7857052cbbdc0600b1584a7d8f1
DerLed <48630545+DerLed@users.noreply.github.com>
2022-06-30 19:52:15 +0300

    init commit

--- src/main/java/ru/lebedev/bank/domain/account/AccountController.java
+++ src/main/java/ru/lebedev/bank/domain/account/AccountController.java
@@ -1,8 +1,12 @@
 package ru.lebedev.bank.domain.account;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/accounts")
+@RequiredArgsConstructor
 public class AccountController {
-
+    private final AccountService accountService;
 }
