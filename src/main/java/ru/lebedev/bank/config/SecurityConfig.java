d883348335b93692e48fa45e10b94f5959435fe3
DerLed <48630545+DerLed@users.noreply.github.com>
2022-07-12 23:27:52 +0300

    заменил конструктор на lombok анотацию

--- src/main/java/ru/lebedev/bank/config/SecurityConfig.java
+++ src/main/java/ru/lebedev/bank/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package ru.lebedev.bank.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -19,14 +18,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
 
-    @Autowired
-    public SecurityConfig(@Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
